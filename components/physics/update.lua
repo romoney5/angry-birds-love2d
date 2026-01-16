44680fef738904d10970bda5bf53dba73abd3764
romoney5 <whygetmugged@gmail.com>
2026-01-16 00:34:55 -0500

    fix trajectory on later versions

--- components/physics/update.lua
+++ components/physics/update.lua
@@ -1,5 +1,44 @@
 --update physics every frame
 
+local function updateTrajectory()
+	-- trajectory
+	local recordTrajectory = false
+	if flyingBird ~= nil then
+		if flyingBird.recordTrajectory ~= false then
+			recordTrajectory = true
+			local lx, ly = physicsToWorldTransform(flyingBird.x, flyingBird.y)
+			local bt = birdTrajectory[1] 
+			if #bt < 1 or vLength(lx - bt[#bt].x, ly - bt[#bt].y) > 20 then
+				-- if getObjectDefinition(flyingBird.name).particlesTrail ~= nil then
+					-- addParticles(flyingBird.name, getObjectDefinition(flyingBird.name).particlesTrail, 1)
+				-- end
+				_G.table.insert(bt, { x = lx, y = ly })
+				addToTrajectory(1, lx, ly)
+			end
+		end
+	end
+	
+	if otherBirds ~= nil then
+		for i = 1, 2 do
+			local obj = objects.world[otherBirds[i]]
+			if obj ~= nil then
+				if obj.recordTrajectory ~= false then
+					recordTrajectory = true
+					local lx, ly = physicsToWorldTransform(obj.x, obj.y)
+					local bt = birdTrajectory[i+1] 
+					if #bt < 1 or vLength(lx - bt[#bt].x, ly - bt[#bt].y) > 20 then
+						-- if getObjectDefinition(obj.name).particlesTrail ~= nil then
+							-- addParticles(obj.name, getObjectDefinition(obj.name).particlesTrail, 1)
+						-- end
+						_G.table.insert(bt, { x = lx, y = ly })
+						addToTrajectory(i+1, lx, ly)
+					end
+				end
+			end
+		end
+	end
+end
+
 function solvePhysics()
 	local timeStep = dt2 * (physicsTimeScale or 1)
 	local velocityIterations = 10
@@ -31,6 +70,9 @@ function updatePhysics(dt)
 	if applyForcesAtPhysicsStep then applyForcesAtPhysicsStep() end
 	if clearLuaForceFunctions then clearLuaForceFunctions() end
 
+	--update the trajectory in the case of a newer version, on older versions the distance check prevents it from running twice
+	updateTrajectory()
+
 	if removeBlocks then
 		removeBlocks()
 	end
@@ -51,7 +93,7 @@ function updatePhysics(dt)
 			obj.x, obj.y = obj.body:getPosition()
 			
 			local xVel, yVel = obj.body:getLinearVelocity()
-			local velMagnitude = xVel^2 +  yVel^2
+			local velMagnitude = xVel^2 + yVel^2
 			local angularVelocity = obj.body:getAngularVelocity()
 			
 			if velMagnitude ~= 0 then
