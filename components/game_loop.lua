fd5fa0a50cc24eb12bdcc3d567f0cf2deb209cf7
romoney5 <93959424+romoney5@users.noreply.github.com>
2026-08-28 20:45:18 -0400

    rio paused hack isn't needed anymore

--- components/game_loop.lua
+++ components/game_loop.lua
@@ -151,12 +151,6 @@ function love.update(dt)
 		
 		keyPressed, keyReleased, keyHold, cursor.wheel = kp, kr, kh, cw
 		
-		-- temporary fix for rio/space
-		if sm and sm.currentScene == sm.scenes.pause then
-			setPhysicsEnabled(false)
-			g_gamePaused = true
-		end
-		
 		updatePhysics(dt)
 
 		zoomLevel = lerp(zoomLevel, wantedZoomLevel, dt * 8)
