60c39a6dc9c2992b8cade24b94b25478205366a0
romoney5 <whygetmugged@gmail.com>
2026-09-05 16:23:33 -0400

    replace buggy drawBoxNative with 2.0.0's drawBox
    i don't actually know why they made a native and lua version
    seeing as they both do the exact same thing

--- components/resources/draw_box.lua
+++ components/resources/draw_box.lua
@@ -1,7 +1,10 @@
---drawboxnative is huge so i put it here
+--drawboxnative is huge so it's here instead
+--edited from 2.0.0's gamelogic.lua
 
 function gamelua.drawBoxNative(boxSprites, x1, y1, width, height, hAnchor, vAnchor, color)
+	local res = _G.res
 	local r, g, b, a = 1.0, 1.0, 1.0, 1.0
+	
 	if color then
 		r = color.red or r
 		g = color.green or g
@@ -12,112 +15,20 @@ function gamelua.drawBoxNative(boxSprites, x1, y1, width, height, hAnchor, vAnch
 	local x2 = x1 + width - 1
 	local y2 = y1 + height - 1
 	
-	local pxTopLeft,   pyTopLeft   = 0, 0
-	local pxTopMiddle, pyTopMiddle = 0, 0
-	local pxTopRight,  pyTopRight  = 0, 0
-	local twTopLeft,   thTopLeft   = 0, 0
-	local twTopMiddle, thTopMiddle = 0, 0
-	local twTopRight,  thTopRight  = 0, 0
+	boxSprites.topLeft = boxSprites.topLeft or ""
+	boxSprites.topRight = boxSprites.topRight or ""
+	boxSprites.bottomLeft = boxSprites.bottomLeft or ""
+	boxSprites.center = boxSprites.center or ""
 	
-	local pxMiddleLeft,  pyMiddleLeft = 0, 0
-	local pxMiddleRight, pyMiddleRight = 0, 0
-	local twMiddleLeft,  thMiddleLeft = 0, 0
-	local twMiddleRight, thMiddleRight = 0, 0
+	boxSprites.topMiddle = boxSprites.topMiddle or ""
+	boxSprites.left = boxSprites.left or ""
+	boxSprites.right = boxSprites.right or ""
+	boxSprites.bottomMiddle = boxSprites.bottomMiddle or ""
 	
-	local pxBottomLeft,   pyBottomLeft = 0, 0
-	local pxBottomMiddle, pyBottomMiddle = 0, 0
-	local pxBottomRight,  pyBottomRight = 0, 0
-	local twBottomLeft,   thBottomLeft = 0, 0
-	local twBottomMiddle, thBottomMiddle = 0, 0
-	local twBottomRight,  thBottomRight = 0, 0
-		
-	if boxSprites.topLeft ~= nil then
-		pxTopLeft, pyTopLeft = _G.res.getSpritePivot(sheet, boxSprites.topLeft)
-		twTopLeft, thTopLeft = _G.res.getSpriteBounds(sheet, boxSprites.topLeft)
-	else
-		boxSprites.topLeft = ""
-	end
-	
-	if boxSprites.topMiddle ~= nil then
-		pxTopMiddle, pyTopMiddle = _G.res.getSpritePivot(sheet, boxSprites.topMiddle)
-		twTopMiddle, thTopMiddle = _G.res.getSpriteBounds(sheet, boxSprites.topMiddle)
-	else
-		boxSprites.topMiddle = ""
-	end
-	
-	if boxSprites.topRight ~= nil then
-		pxTopRight,  pyTopRight  = _G.res.getSpritePivot(sheet, boxSprites.topRight)
-		twTopRight,  thTopRight  = _G.res.getSpriteBounds(sheet, boxSprites.topRight)
-	else
-		boxSprites.topRight = ""
-	end
-	
-	if boxSprites.left ~= nil then
-		pxMiddleLeft, pyMiddleLeft = _G.res.getSpritePivot(sheet, boxSprites.left)
-		twMiddleLeft, thMiddleLeft = _G.res.getSpriteBounds(sheet, boxSprites.left)
-	else
-		boxSprites.left = ""
-	end
-	
-	if boxSprites.right ~= nil then 
-		pxMiddleRight, pyMiddleRight = _G.res.getSpritePivot(sheet, boxSprites.right)
-		twMiddleRight, thMiddleRight = _G.res.getSpriteBounds(sheet, boxSprites.right)
-	else
-		boxSprites.right = ""
-	end
-	
-	if boxSprites.bottomLeft ~= nil then
-		pxBottomLeft, pyBottomLeft  = _G.res.getSpritePivot(sheet, boxSprites.bottomLeft)
-		twBottomLeft, thBottomLeft  = _G.res.getSpriteBounds(sheet, boxSprites.bottomLeft)
-	else
-		boxSprites.bottomLeft = ""
-	end
-	
-	if boxSprites.bottomMiddle ~= nil then
-		pxBottomMiddle, pyBottomMiddle = _G.res.getSpritePivot(sheet, boxSprites.bottomMiddle)
-		twBottomMiddle, thBottomMiddle = _G.res.getSpriteBounds(sheet, boxSprites.bottomMiddle)
-	else
-		boxSprites.bottomMiddle = ""
-	end
-	
-	if boxSprites.bottomRight ~= nil then
-		pxBottomRight,  pyBottomRight  = _G.res.getSpritePivot(sheet, boxSprites.bottomRight)
-		twBottomRight,  thBottomRight  = _G.res.getSpriteBounds(sheet, boxSprites.bottomRight)
-	else
-		boxSprites.bottomRight = ""
-	end
-
-	if boxSprites.center == nil then
-		boxSprites.center = ""
-	end
-	
-	local startXTopMiddle = x1 + twTopLeft - pxTopLeft
-	local stopXTopMiddle = x2 - pxTopRight
-	local y1Top = y1 - pyTopMiddle
-	if stopXTopMiddle < startXTopMiddle then
-		stopXTopMiddle = startXTopMiddle 
-	end
-	
-	local startXBottomMiddle = x1 + twBottomLeft - pxBottomLeft
-	local stopXBottomMiddle = x2 - pxBottomRight
-	local y1Bottom = y2 - pyBottomMiddle
-	if stopXBottomMiddle < startXBottomMiddle then
-		stopXBottomMiddle = startXBottomMiddle 
-	end
-	
-	local startYMiddleLeft = y1 + thTopLeft - pyTopLeft
-	local stopYMiddleLeft = y2 - pyBottomLeft
-	local x1Left = x1 - pxMiddleLeft
-	if stopYMiddleLeft < startYMiddleLeft then
-		stopYMiddleLeft = startYMiddleLeft 
-	end
-	
-	local startYMiddleRight = y1 + thTopRight - pyTopRight
-	local stopYMiddleRight = y2 - pyBottomRight
-	local x1Right = x2 - pxMiddleRight
-	if stopYMiddleRight < startYMiddleRight then
-		stopYMiddleRight = startYMiddleRight 
-	end
+	local _, thTopMiddle = res.getSpriteBounds(boxSprites.topMiddle)
+	local twMiddleLeft, _ = res.getSpriteBounds(boxSprites.left)
+	local twMiddleRight, _ = res.getSpriteBounds(boxSprites.right)
+	local _, thBottomMiddle = res.getSpriteBounds(boxSprites.bottomMiddle)
 	
 	local xPivot, yPivot = 0, 0
 	
@@ -128,6 +39,7 @@ function gamelua.drawBoxNative(boxSprites, x1, y1, width, height, hAnchor, vAnch
 	else -- left
 		xPivot = 0
 	end
+	
 	if vAnchor == "VCENTER" then
 		yPivot = -height / 2
 	elseif vAnchor == "BOTTOM" then
@@ -135,23 +47,26 @@ function gamelua.drawBoxNative(boxSprites, x1, y1, width, height, hAnchor, vAnch
 	else -- top
 		yPivot = 0
 	end
+	
+	-- check if some part of the box is on screen
+	if y1 - thTopMiddle + yPivot <= gamelua.screenHeight and y1 + height + yPivot + thBottomMiddle >= 0 then
+		-- draw borders
+		res.drawSprite(boxSprites.topMiddle, x1 + xPivot , y1 - thTopMiddle + yPivot, "TOP", "LEFT", width, thTopMiddle)
+		res.drawSprite(boxSprites.bottomMiddle, x1 + xPivot , y1 + height + yPivot, "TOP", "LEFT", width, thBottomMiddle)
+		res.drawSprite(boxSprites.left, x1 - twMiddleLeft + xPivot , y1 + yPivot, "TOP", "LEFT", twMiddleLeft, height)
+		res.drawSprite(boxSprites.right, x1 + width + xPivot, y1 + yPivot, "TOP", "LEFT", twMiddleRight, height)
 		
-	-- draw borders
-	_G.res.drawSprite(boxSprites.topMiddle, _G.math.floor(startXTopMiddle + xPivot) , _G.math.floor(y1Top + yPivot) , "TOP", "LEFT", _G.math.floor(stopXTopMiddle - startXTopMiddle), _G.math.floor(thTopMiddle))
-	_G.res.drawSprite(boxSprites.bottomMiddle, _G.math.floor(startXBottomMiddle + xPivot) , _G.math.floor(y1Bottom + yPivot), "TOP", "LEFT", _G.math.floor(stopXBottomMiddle - startXBottomMiddle), _G.math.floor(thBottomMiddle))
-	_G.res.drawSprite(boxSprites.left, _G.math.floor(x1Left + xPivot) , _G.math.floor(startYMiddleLeft + yPivot) , "TOP", "LEFT", _G.math.floor(twMiddleLeft), _G.math.floor(stopYMiddleLeft - startYMiddleLeft))
-	_G.res.drawSprite(boxSprites.right, _G.math.floor(x1Right + xPivot), _G.math.floor(startYMiddleRight + yPivot) , "TOP", "LEFT", _G.math.floor(twMiddleRight), _G.math.floor(stopYMiddleRight - startYMiddleRight))
-	
-	-- draw corners
-	_G.res.drawSprite(boxSprites.topLeft, _G.math.floor(x1 + xPivot), _G.math.floor(y1 + yPivot))
-	_G.res.drawSprite(boxSprites.topRight, _G.math.floor(x2 + xPivot), _G.math.floor(y1 + yPivot))
-	_G.res.drawSprite(boxSprites.bottomLeft, _G.math.floor(x1 + xPivot), _G.math.floor(y2 + yPivot))
-	_G.res.drawSprite(boxSprites.bottomRight, _G.math.floor(x2 + xPivot), _G.math.floor(y2 + yPivot))
-	
-	-- if color isn't defined then fill with center sprite
-	if color ~= nil then
-		gamelua.drawRect(r, g, b, a, _G.math.floor(x1 + xPivot), _G.math.floor(y1 + yPivot), _G.math.floor(x2 + xPivot), _G.math.floor(y2 + yPivot), false)
-	else
-		_G.res.drawSprite(boxSprites.center, _G.math.floor(x1 + xPivot), _G.math.floor(y1 + yPivot), "TOP", "LEFT", _G.math.floor(width), _G.math.floor(height))
+		-- draw corners
+		res.drawSprite(boxSprites.topLeft, x1 + xPivot, y1 + yPivot, "BOTTOM", "RIGHT")
+		res.drawSprite(boxSprites.topRight, x1 + width + xPivot, y1 + yPivot, "BOTTOM", "LEFT")
+		res.drawSprite(boxSprites.bottomLeft, x1 + xPivot, y1 + height + yPivot, "TOP", "RIGHT")
+		res.drawSprite(boxSprites.bottomRight, x1 + width + xPivot, y1 + height + yPivot, "TOP", "LEFT")
+		
+		-- if color isn't defined then fill with center sprite
+		if color ~= nil then
+			gamelua.drawRect(r, g, b, a, x1 + xPivot, y1 + yPivot, x2 + xPivot, y2 + yPivot, false)
+		else
+			res.drawSprite(boxSprites.center, x1 + xPivot, y1 + yPivot, "TOP", "LEFT", width, height)
+		end
 	end
 end
